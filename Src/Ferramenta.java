a9e6ccd86acf124d34d81e38dc62dd289fef53ac
ViniciusInCode <devvine75@gmail.com>
2024-11-12 14:15:13 -0300

    Adiciona comentários explicativos na classe Ferramenta para melhorar a legibilidade e compreensão do código

--- Src/Ferramenta.java
+++ Src/Ferramenta.java
@@ -1,16 +1,20 @@
 // Ferramenta.java
 public class Ferramenta extends Produto {
+    // Atributo que define o tipo da ferramenta
     private String tipo;
 
+    // Construtor da classe Ferramenta
     public Ferramenta(int id, String nome, double preco, String tipo) {
-        super(id, nome, preco);
-        this.tipo = tipo;
+        super(id, nome, preco); // Chama o construtor da classe pai Produto
+        this.tipo = tipo; // Inicializa o atributo tipo
     }
 
+    // Método getter para o atributo tipo
     public String getTipo() {
         return tipo;
     }
 
+    // Método toString para representar a Ferramenta como uma string
     @Override
     public String toString() {
         return "Ferramenta [ID=" + getId() + ", Nome=" + getNome() + ", Preço=" + getPreco() + ", Tipo=" + tipo + "]";
